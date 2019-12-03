bf6c27e5085c1ad21be4b5882b8c12953b55380d
Timur Kapitonov <kapitonov.timur@gmail.com>
2019-12-04 00:19:24 +0300

    created profile, possibility to edit it

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 def user_login(request):
     if request.method == 'POST':
@@ -32,11 +33,26 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            new_user = user_form.save(commit = False)
+            new_user = user_form.save()
             new_user.set_password(user_form.cleaned_data['password'])
+            Profile.objects.create(user=new_user)
             new_user.save()
             return render(request, 'account/register_done.html', {'new_user':new_user})
     else:
         user_form = UserRegistrationForm()
 
     return render (request, 'account/register.html', {'user_form':user_form})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance = request.user, data = request.POST)
+        profile_form = ProfileEditForm(instance = request.user.profile, data = request.POST, files = request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance = request.user)
+        profile_form = ProfileEditForm(instance = request.user.profile)
+
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
