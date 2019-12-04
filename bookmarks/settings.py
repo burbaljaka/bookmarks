1dc9203dc9be83a88c70bcc64dd1679fb9fab6ac
Timur Kapitonov <kapitonov.timur@gmail.com>
2019-12-04 09:25:08 +0300

    started working with social_django

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a_p#$%%ff09+)u)sf36$@+@e!hkni=dc9y+iy!o=7i#wmvsy$p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'social_django'
 ]
 
 MIDDLEWARE = [
